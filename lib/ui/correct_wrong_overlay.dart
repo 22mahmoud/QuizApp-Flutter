70c94e8802125a2352eb073fd163eec73aa1518f
22mahmoud <mahmouds1997@yahoo.com>
2018-03-01 00:38:59 +0200

    add correct Wrong Overlay UI

---
+++ lib/ui/correct_wrong_overlay.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class CorrectWorngOverlay extends StatefulWidget {
+  final bool _isCorrect;
+
+  CorrectWorngOverlay(this._isCorrect);
+
+  @override
+  State createState() => new CorrectWorngOverlayState();
+}
+
+class CorrectWorngOverlayState extends State<CorrectWorngOverlay>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        duration: new Duration(seconds: 2), vsync: this);
+    _iconAnimation = new CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.elasticOut);
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+        color: Colors.black54,
+        child: new InkWell(
+          onTap: () => print('new Text Overlay'),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                  decoration: new BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: new Transform.rotate(
+                    child: new Icon(
+                      widget._isCorrect ? Icons.done : Icons.clear,
+                      size: _iconAnimation.value * 80.0,
+                    ),
+                    angle: _iconAnimation.value * 2 * PI,
+                  )),
+              new Padding(
+                padding: new EdgeInsets.only(bottom: 20.0),
+              ),
+              new Text(
+                widget._isCorrect ? 'Correct!' : "Wrong!",
+                style: new TextStyle(color: Colors.white, fontSize: 30.0),
+              )
+            ],
+          ),
+        ));
+  }
+}
